374d3dceaece527dbb57dd9aef7b7068ebab87bf
Byron Bacusoy Pinela <ByronBP@macbook-pro-de-byron.local>
2017-05-17 21:32:26 +0200

    initialCommit

---
+++ simpleClockBoxingV1.1/BoxingClockView.swift
@@ -0,0 +1,21 @@
+//
+//  BoxingClockView.swift
+//  simpleClockBoxingV1.1
+//
+//  Created by Byron Bacusoy Pinela on 17/5/17.
+//  Copyright © 2017 Novex. All rights reserved.
+//
+
+import UIKit
+
+class BoxingClockView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
